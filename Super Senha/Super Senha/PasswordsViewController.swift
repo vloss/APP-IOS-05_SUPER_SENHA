bc7cf762614f2440eca65d4a10831b3c5ae14b0e
vloss <viniciusvianaloss@gmail.com>
2023-01-15 14:38:41 -0300

    Continuação de App.

---
+++ Super Senha/Super Senha/PasswordsViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PasswordsViewController.swift
+//  Super Senha
+//
+//  Created by Vinicius Loss on 15/01/23.
+//
+
+import UIKit
+
+class PasswordsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Super Senha/Super Senha/ViewController.swift
+++ Super Senha/Super Senha/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tfTotalPasswords: UITextField!
+    @IBOutlet weak var tfNumberOfCharacters: UITextField!
+    @IBOutlet weak var swSpecialCharacters: UISwitch!
+    @IBOutlet weak var swCapitalLetters: UISwitch!
+    @IBOutlet weak var swNumbers: UISwitch!
+    @IBOutlet weak var swLetters: UISwitch!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
